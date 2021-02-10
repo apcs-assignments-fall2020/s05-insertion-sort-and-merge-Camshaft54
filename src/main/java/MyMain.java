1fec473c5ff780835c39691bbf9cd14e2f802ee6
camshaft54 <cam.shaw1234@gmail.com>
2021-02-09 20:37:43 -0800

    Change merge method so that it does the merging the way it's supposed to

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class MyMain {
 
@@ -29,23 +27,30 @@ public class MyMain {
      // sorted array
      // You may assume arr1 and arr2 are the same length
     public static int[] merge(int[] arr1, int[] arr2) {
-        int[] arr = new int[arr1.length + arr2.length];
-        System.arraycopy(arr1, 0, arr, 0, arr1.length);
-        System.arraycopy(arr2, 0, arr, arr1.length, arr2.length);
-        System.out.println(Arrays.toString(arr));
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i : arr) {
-            list.add(i);
+        int[] arr = new int[arr1.length * 2];
+        int count1 = 0;
+        int count2 = 0;
+        while (count1 + count2 < arr.length) {
+            if (count2 == arr2.length) {
+                arr[count1+count2] = arr1[count1];
+                count1++;
+            } else if (count1 == arr1.length) {
+                arr[count2+count1] = arr2[count2];
+                count2++;
+            } else {
+                if (arr1[count1] < arr2[count2]) {
+                    arr[count1+count2] = arr1[count1];
+                    count1++;
+                } else {
+                    arr[count1+count2] = arr2[count2];
+                    count2++;
+                }
+            }
         }
-        int[] sorted = new int[arr.length];
-        list = insertionSort(list);
-        for (int i = 0; i < arr.length; i++) {
-            sorted[i] = list.get(i);
-        }
-        return sorted;
+        return arr;
     }
 
     public static void main(String[] args) {
-        merge(new int[]{1, 2}, new int[]{0, 3});
+        System.out.println(Arrays.toString(merge(new int[]{1, 2}, new int[]{0, 3})));
     }
 }
